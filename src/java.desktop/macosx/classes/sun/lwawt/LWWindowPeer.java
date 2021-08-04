9324a27931b43b0df67a8836f4d2fe4439c77947
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2021-08-04 21:02:21 +0200

    8266079: Lanai: AlphaComposite shows differences on Metal compared to OpenGL
    Update opacity on textured property change

--- src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -520,7 +520,10 @@ public class LWWindowPeer
     }
 
     public final void setTextured(final boolean isTextured) {
-        textured = isTextured;
+        if (textured != isTextured) {
+            textured = isTextured;
+            updateOpaque();
+        }
     }
 
     @Override
